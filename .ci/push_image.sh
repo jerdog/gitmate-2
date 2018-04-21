07708f168ea897b7d75ef8bdf1fd9033c57d2b65
Muhammad Kaisar Arkhan <yukiisbored@disroot.org>
2018-04-21 21:53:13 +0700

    Improve CI scripts
    Mangle scripts that are in-line in the CI configuration into shell
    scripts to reduce line number and duplicates.
    
    Run tests inside the freshly built Docker image to ensure it's working
    as properly.

--- .ci/push_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x -e -o pipefail
-
-SOURCE="$1"
-TARGET="$2"
-
-docker pull $SOURCE || docker build -t $SOURCE .
-docker tag $SOURCE $TARGET
-docker push $TARGET
--- .ci/send_mattermost.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-exec curl -X POST \
-          -H 'Content-Type: application/json' \
-          -d "{ \"text\": \"$2\" }" \
-          "$1"
