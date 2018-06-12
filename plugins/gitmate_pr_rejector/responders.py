ad5480c9b0a124d98020a4ad3fd3caf20fa1e321
Vamshi99 <bommvams72@gmail.com>
2018-06-12 14:30:23 +0530

    pr_rejector: Support regex in branch names

--- plugins/gitmate_pr_rejector/responders.py
+++ plugins/gitmate_pr_rejector/responders.py
@@ -1,3 +1,5 @@
+import re
+
 from IGitt.Interfaces.Actions import MergeRequestActions
 from IGitt.Interfaces.MergeRequest import MergeRequest
 from IGitt.Interfaces import MergeRequestStates
@@ -14,8 +16,8 @@ def reject_pull_requests(pr: MergeRequest,
                          branch_names: list = ['master'],
                          message: str = 'Open PR on a different source branch '
                          'other than master.'):
-
-    if pr.head_branch_name in branch_names:
-        if pr.state == MergeRequestStates.OPEN:
+    for branch in branch_names:
+        if (re.fullmatch(branch, pr.head_branch_name) and
+                pr.state == MergeRequestStates.OPEN):
             pr.add_comment(message)
             pr.close()
--- plugins/gitmate_pr_rejector/tests/test_gitmate_pr_rejector.py
+++ plugins/gitmate_pr_rejector/tests/test_gitmate_pr_rejector.py
@@ -34,6 +34,36 @@ class TestGitmatePrRejector(GitmateTestCase):
                                          ' other than master.')
         m_close.assert_called_once()
 
+    @patch.object(GitHubMergeRequest, 'head_branch_name',
+                  new_callable=PropertyMock)
+    @patch.object(GitHubMergeRequest, 'state', new_callable=PropertyMock)
+    @patch.object(GitHubMergeRequest, 'close')
+    @patch.object(GitHubMergeRequest, 'add_comment')
+    def test_github_with_regex(self, m_add_comment, m_close, m_state,
+                               m_head_branch_name):
+        data = {
+            'repository': {'full_name': environ['GITHUB_TEST_REPO'],
+                           'id': 49558751},
+            'pull_request': {'number': 2},
+            'action': 'opened'
+        }
+        self.repo.settings = [{
+            'name': 'pr_rejector',
+            'settings': {
+                'branch_names': ['release\/.*'],
+                'message': 'Open PR on a different source branch that '
+                           'does not start with `release/`'
+            }
+        }]
+        m_head_branch_name.return_value = 'release/1.0'
+        m_state.return_value = MergeRequestStates.OPEN
+        response = self.simulate_github_webhook_call('pull_request', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        m_add_comment.assert_called_with('Open PR on a different source branch'
+                                         ' that does not start with `release/`'
+                                         )
+        m_close.assert_called_once()
+
     @patch.object(GitLabMergeRequest, 'head_branch_name',
                   new_callable=PropertyMock)
     @patch.object(GitLabMergeRequest, 'state', new_callable=PropertyMock)
@@ -54,4 +84,36 @@ class TestGitmatePrRejector(GitmateTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         m_add_comment.assert_called_with('Open PR on a different source branch'
                                          ' other than master.')
+        m_close.assert_called_once()
+
+    @patch.object(GitLabMergeRequest, 'head_branch_name',
+                  new_callable=PropertyMock)
+    @patch.object(GitLabMergeRequest, 'state', new_callable=PropertyMock)
+    @patch.object(GitLabMergeRequest, 'close')
+    @patch.object(GitLabMergeRequest, 'add_comment')
+    def test_gitlab_with_regex(self, m_add_comment, m_close, m_state,
+                               m_head_branch_name):
+        data = {
+            'object_attributes': {
+                'target': {'path_with_namespace': environ['GITLAB_TEST_REPO']},
+                'action': 'open',
+                'iid': 25
+            }
+        }
+        self.gl_repo.settings = [{
+            'name': 'pr_rejector',
+            'settings': {
+                'branch_names': ['release\/.*'],
+                'message': 'Open PR on a different source branch that '
+                           'does not start with `release/`'
+            }
+        }]
+        m_head_branch_name.return_value = 'release/1.0'
+        m_state.return_value = MergeRequestStates.OPEN
+        response = self.simulate_gitlab_webhook_call(
+            'Merge Request Hook', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        m_add_comment.assert_called_with('Open PR on a different source branch'
+                                         ' that does not start with `release/`'
+                                         )
         m_close.assert_called_once()
