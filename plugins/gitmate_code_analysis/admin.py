fe6db8f7497069e333d4c98b978763ac0ac0739b
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-11-07 01:46:32 +0530

    Remove unused admin.py files

--- plugins/gitmate_code_analysis/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from gitmate_config.admin.utils import DisplayAllAdmin
-from .models import AnalysisResults
-
-
-admin.site.register(AnalysisResults, DisplayAllAdmin)
